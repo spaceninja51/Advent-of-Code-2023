b9e7d7c26998d116ad61ac3b5f1facfcd4b17ccc
spaceninja51 <cwg2423@gmail.com>
2024-01-25 15:17:19 -0500

    Cleanup

--- Day 2/2-1 Script.ps1
+++ Day 2/2-1 Script.ps1
@@ -1,19 +1,13 @@
-# Path to the text file
+# Path to the input file
 $file = "C:\Users\cwg24\OneDrive\Desktop\Coding\Advent of Code 2023\Day 2\input.txt"
 
 # Read the file content as an array of lines
 $dataSet = Get-Content -Path $file
 
-# 12 red cubes, 13 green cubes, and 14 blue cubes
-$redMax = 12
-$greenMax = 13
-$blueMax = 14
-
 foreach ($line in $dataSet) {
-    [int]$gameid = 0
+
     # Extract just the round text
-    $roundText = $line.Substring($line.indexOf(':')) -replace ': '
-    Write-Output $roundText
+
     # Break the line down into rounds
 
    # Break each round into its cube counts
@@ -22,5 +16,4 @@ foreach ($line in $dataSet) {
 
    # If so add the game ID to a running total
    
-    $gameid = $line.Substring(0,$line.indexOf(':')) -replace '\D'
 }
