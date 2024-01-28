f53f23ce60604b296805e433b82e998a5a70658e
spaceninja51 <cwg2423@gmail.com>
2024-01-28 18:48:17 -0500

    uhhhh

--- Day 2/2-1/Script.ps1
+++ Day 2/2-1/Script.ps1
@@ -1,49 +1,44 @@
-<# $text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" #>
-
-$text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+#$text = "Game 1: 1 red, 2 green, 6 blue"
+$text = Get-Content "C:\Users\cwg24\OneDrive\Desktop\Nerd Shit\Scripting\Advent of Code 2023\Day 2\2-1\Input.txt"
 class Round {
-    [int] $red = 0
-    [int] $green = 0
-    [int] $blue = 0
-    [int] $total = 0
+    [int] $red
+    [int] $green
+    [int] $blue
+    [int] $total
 }
 
 # Iterate through each line of the input text and break the line into the two strings made of the text on either side of :
 foreach ($line in $text.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)) {
 
-    $gameID = $line.split(":")[0] -replace '[a-zA-Z]',''
+    $lineText = $line.Split(":")
+    $gameID = $lineText[0] -replace '[a-zA-Z]',''
     
     foreach($pull in $lineText[1].Split(";")) {
         # create a new round object after each ;
         $round = [Round]::new()
         
-        foreach ($count in $pull.Split(",")) {
-            $count
-            # find each color count in a round and set their values
-            switch ($count.Contains((' red')), ($count.Contains(' green')), ($count.Contains(' blue'))) {
-                ($count.contains('red')) {
-                    $round.red = $count -replace '[a-zA-Z]',''
-                    $count = "null"
+        foreach ($count in $pull.Split(",").Trim()) {
+            # check the number to be set
+            [int] $countNo = $count.Split(" ")[0].Trim()
+
+            # find the color the number goes to and set it
+            $countColor = $count.Split(" ")[1].Trim()
+            switch ($countColor) {
+                "red" {
+                    $round.red = $countNo
                     break
                 }
-                ($count.contains('green')) {
-                    $round.green = $count -replace '[a-zA-Z]',''
-                    $count = "null"
+                "green" {
+                    $round.green = $countNo
                     break
                 }
-                ($count.contains('blue')) {
-                    $round.blue = $count -replace '[a-zA-Z]',''
-                    $count = "null"
+                "blue" {
+                    $round.blue = $countNo
                     break
                 }
             }
         }
         # calculate total cubes shown in a round
         $round.total = $round.red + $round.green + $round.blue
-    
     }
 }
