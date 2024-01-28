e3c79e99350cd9f975dadd44a030325b5cdf4930
spaceninja51 <cwg2423@gmail.com>
2024-01-27 22:26:18 -0500

    Can now pull game ID of each line

--- Day 2/2-1 Script.ps1
+++ Day 2/2-1 Script.ps1
@@ -1,26 +1,35 @@
-# Path to the text file
-$file = "C:\Users\cwg24\OneDrive\Desktop\Coding\Advent of Code 2023\Day 2\input.txt"
+<# Test Data:
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
-# Read the file content as an array of lines
-$dataSet = Get-Content -Path $file
+Games 1,2,and 5 are possible so should get 8
 
-# 12 red cubes, 13 green cubes, and 14 blue cubes
-$redMax = 12
-$greenMax = 13
-$blueMax = 14
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games? #>
 
-foreach ($line in $dataSet) {
-    [int]$gameid = 0
-    # Extract just the round text
-    $roundText = $line.Substring($line.indexOf(':')) -replace ': '
-    Write-Output $roundText
-    # Break the line down into rounds
+$file = "C:\Users\cwg24\OneDrive\Desktop\Nerd Shit\Coding\Advent-of-Code-2023\Day 2\2-1 and 2-2 Input.txt"
 
-   # Break each round into its cube counts
+$text = Get-Content $file
 
-   # Check if they're all possible
+<# $text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" #>
 
-   # If so add the game ID to a running total
-   
-    $gameid = $line.Substring(0,$line.indexOf(':')) -replace '\D'
+# Set maximum color values
+$rmax = 12
+$gmax = 13
+$bmax = 14
+# Set maximum total cubes by adding the max of each color
+$tmax = $rmax + $gmax + $bmax
+
+# Iterate through each line of the input text and break the line into the two strings made of the text on either side of :
+foreach ($entry in $text) {
+    $lineText = $entry.split(":")
+    
+    # Set the game's ID by refereincing the text before :, then removing "Game "
+    [int] $gameID = $lineText[0].Trim("Game ")
 }
