ff233eae133b943b2efac82fe0762c3633a84752
spaceninja51 <cwg2423@gmail.com>
2024-01-29 16:57:07 -0500

    Went from only getting 720 to getting 0, both wrong

--- Day 2/2-2/Script.ps1
+++ Day 2/2-2/Script.ps1
@@ -1,6 +1,5 @@
 <# Create a class used to represent an individual round of the game #>
 class Round {
-    [int] $ID
     [int] $red
     [int] $green
     [int] $blue
@@ -9,68 +8,60 @@ class Round {
 <# create a class used to represent each game (line) and track its color reauirements while storing each round's data #>
 class Game {
     [int] $ID
-    [int] $bmin = 0
-    [int] $gmin = 0
-    [int] $rmin = 0
-    [int] $Power = 0
+    [int] $bmin
+    [int] $gmin
+    [int] $rmin
+    [int] $Power
     [Array] $Rounds
 }
 <# initialize variables #>
-$gameID, $answer1 = 0
+$answer2 = 0
 
 <# data to be processed #>
 $file = "C:\Users\Curtis\OneDrive\Desktop\Coding\Scripting\Advent of Code 2023\Advent-of-Code-2023\Day 2\Input.txt"
 $text = Get-Content $file
 
-<# Iterate through each line of the input text and break the line into the two strings made of the text on either side of : #>
-
+<# Looping through each game #>
 foreach ($line in $text.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)) {
-    
-    $currentGame = [Game]::new()
+    <# Iterate through each line of the input text and break the line into the two strings made of the text on either side of :.#>
     $lineText = $line.Split(":")
+
+    <# Create new Game object at every new line set its ID based on that game's number#>
+    $currentGame = [Game]::new()
     $currentGame.ID = $lineText[0] -replace '[a-zA-Z]',''
-    $roundNum, $rmin, $gmin, $bmin = 0
 
+    <# resetting variables for the next game #>
+    $rmin, $gmin, $bmin = 0
+    
+    <# Looping through each round#>
     foreach($pull in $lineText[1].Split(";")) {
-        <# create a new round object after each ;. For part 2, keep track of round number as well, resetting after each line #>
+        <# create a new round object with each ; #>
         $currentRound = [Round]::new()
-        $roundNum++
-        $currentRound.ID = $roundNum
 
+        <# Loop through each count in a round #>
         foreach ($count in $pull.Split(",").Trim()) {
-            <# check the number to be set #>
+            
+            <# Split the counts into number:color and set variables like $countColor:$countNo #>
             [int] $countNo = $count.Split(" ")[0].Trim()
-
-            <# find the color the number goes to and compares it to the existing max for the corresponding color, if it is the new max set it as so #>
             $countColor = $count.Split(" ")[1].Trim()
+
+            <# find the color the number goes to and set its corresponding value #>
             switch ($countColor) {
                 "red" {
                     $currentRound.red = $countNo
-                    if ($countNo -gt $currentGame.rmin) {
-                        $currentGame.rmin = $countNo
                     }
-                }
                 "green" {
                     $currentRound.green = $countNo
-                    if ($countNo -gt $currentGame.gmin) {
-                        $currentGame.gmin = $countNo
-                    }
                 }
                 "blue" {
                     $currentRound.blue = $countNo
-                    if ($countNo -gt $currentGame.bmin) {
-                        $currentGame.bmin = $countNo
                     }
-                    
                 }
             }
         }
         
-        <# adds the filled out current round to an array of rounds for the current game#>
+        <# adds $currentRound to an array of rounds for the current game #>
         $currentGame.Rounds += $currentRound
-        
-        $currentGame.Power = $currentGame.rmin * $currentGame.gmin * $currentGame.bmin
-        $answer2 += $currentGame.Power 
-
+        $currentGame.Power = $currentGame.rmax * $currentGame.gmax * $currentGame.bmax
+        $answer2 += $currentGame.Power
     }
-}
