fddbb517d6ce6dc2573fed16af4d78626555cf0f
spaceninja51 <cwg2423@gmail.com>
2024-02-14 23:29:04 -0500

    Keeping track of line number

--- Day 3/3-1/Script V1.0.ps1
+++ Day 3/3-1/Script V1.0.ps1
@@ -1,27 +1,25 @@
 $text = Get-Content "Day 3\test.txt"
 
-foreach ($line in $text) {
+$lineNo = 0
+$numPos, $symPos = @()
 
-    <# Selects the numbers in the current line and stores their positions #>
-    $num = $line | Where-Object "[0-9]"
-    $numPos = $line.Matches.Index
+foreach ($line in $text) {
     
-    <# Selects the symbols in the current line and stores their positions #>
-    $sym = $line | Select-String -Pattern "\W"
-    $symPos = $line.Matches.Index
+    <# Tracks the line number #>
+    $lineNo++
     
     <# Checks the lines before and after the current, and if blank sets its variable
     to a line with no numbers or symbols. Otherwise the next and previous lines are 
     set respectively #>
-    if ($null -eq $text[$_ + 1]) {
+    if ($null -eq $text[$lineNo + 1]) {
         $nextLine = "."*($line.Length)
     }
-    elseif ($null -eq $text[$_ - 1]) {
+    elseif ($null -eq $text[$lineNo - 1]) {
         $prevLine = "."*($line.Length)
     }
     else {
-        $nextLine = $text[$_ + 1]
-        $prevLine = $text[$_ - 1]
+        $nextLine = $text[$lineNo + 1]
+        $prevLine = $text[$lineNo - 1]
     }
 
 }
